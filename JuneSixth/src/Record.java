45c10a793ff26caabab6b0022b24ce4baec4181f
unknown <akavmvkdldj1@naver.com>
2019-06-06 16:10:50 +0900

    record

--- JuneSixth/src/Record.java
+++ JuneSixth/src/Record.java
@@ -1,4 +1,46 @@
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
 
-public class Record {
-
+public class Record extends JFrame{
+	public static final int WIDTH = 300;
+	public static final int WEIGHT = 200;
+	public static final int RECORD_NUM = 10;
+	int [] record = new int [10]; //상위 record 10개만 읽어온다고 가정.
+	
+	public Record() {
+		super("Record");
+		setSize(WIDTH, WEIGHT);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setLayout(new BorderLayout());
+		//레코드 파일 읽어오기
+		try {			
+			ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("record"));
+			record = (int[])inputStream.readObject();
+			inputStream.close();
+		}
+		catch(FileNotFoundException e) {
+			System.exit(0);
+		}
+		catch(ClassNotFoundException e) {
+			System.exit(0);
+		}
+		catch(IOException e) {
+			System.exit(0);
+		}
+		//레코드 제목
+		JLabel TextLabel =new JLabel("      RECORD"); 
+		TextLabel.setFont(new Font("Serif", Font.BOLD, 19));
+		add(TextLabel, BorderLayout.NORTH);
+		//레코드 출력
+		JPanel RecordPanel = new JPanel();
+		RecordPanel.setLayout(new GridLayout(RECORD_NUM/2,2));
+		JLabel[] scoreL = new JLabel[RECORD_NUM];
+		for(int i=0; i<scoreL.length; i++)
+			scoreL[i] = new JLabel(Integer.toString(record[i]));
+		for(int i=0; i<scoreL.length; i++)
+			RecordPanel.add(scoreL[i]);
+		add(RecordPanel, BorderLayout.CENTER);
+	}
 }
