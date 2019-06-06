dde0a6ea4c074b864347c0ed7244689b083a1ea6
ohaeori <0typhoon416@gmail.com>
2019-06-06 17:09:17 +0900

    Update comments of Menu.java

--- JuneSixth/src/Menu.java
+++ JuneSixth/src/Menu.java
@@ -11,83 +11,74 @@ import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class Menu extends JFrame implements ActionListener{
-	
+public class Menu extends JFrame implements ActionListener {
 
 	private JButton start;
 	private JButton exit;
 	private JButton showRec;
-	
+
 	private JPanel buttonPan;
-	
-	
-	public Menu()
-	{
+
+	public Menu() {
 		super();
-		setTitle("뭐라고 하지");
-		 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("JuneSixth");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(800, 400);
-		 setLayout(new BorderLayout( ));
-		 
-		 JTextField Title = new JTextField("Game Title");
-		 Title.setFont(new Font("Serif", Font.PLAIN, 100));
-		 Title.setEditable(false);
-		 Title.setBackground(Color.white);
-		 
-		 add(Title,BorderLayout.NORTH);
-		 
-		 JPanel empty1 = new JPanel();
-		 add(empty1,BorderLayout.EAST);
-		 
-		 JPanel empty2 = new JPanel();
-		 add(empty2,BorderLayout.WEST);
-		 
-		 buttonPan = new JPanel(new GridLayout(3,1));
-		 
-		 start = new JButton("start");
-		 start.addActionListener(this);
-		 buttonPan.add(start);
-		 showRec = new JButton("record");
-		 showRec.addActionListener(this);
-		 buttonPan.add(showRec);
-		 exit = new JButton("exit");
-		 exit.addActionListener(this);
-		 buttonPan.add(exit);
-		 add(buttonPan,BorderLayout.CENTER);
+		setLayout(new BorderLayout());
 
-		
-		
+		JTextField Title = new JTextField("Very good enjoy JuneSixth");//ingame title
+		Title.setFont(new Font("Serif", Font.PLAIN, 80));
+		Title.setEditable(false);
+		Title.setBackground(Color.white);
+
+		add(Title, BorderLayout.NORTH);
+
+		JPanel empty1 = new JPanel();//blank
+		add(empty1, BorderLayout.EAST);
+
+		JPanel empty2 = new JPanel();
+		add(empty2, BorderLayout.WEST);//blank
+
+		buttonPan = new JPanel(new GridLayout(3, 1));//sellect mode button
+
+		start = new JButton("start");//start button
+		start.addActionListener(this);
+		buttonPan.add(start);
+		showRec = new JButton("record");//show record button
+		showRec.addActionListener(this);
+		buttonPan.add(showRec);
+		exit = new JButton("exit");//exit game button
+		exit.addActionListener(this);
+		buttonPan.add(exit);
+		add(buttonPan, BorderLayout.CENTER);
+
 	}
-	
-	public void actionPerformed(ActionEvent e)
-    {
-       
-		  String actionCommand = e.getActionCommand( );
-		  
-		  if(actionCommand.equals("start"))
-		  {
-			  
-		  }
-		  else if(actionCommand.equals("record"))
-		  {
-			  
-		  }
-		  
-		  else if(actionCommand.equals("exit"))
-		  {
-			  System.exit(0);
-		  }
-		  else
-		  {
-			  System.err.println("Error");
-		  }
-    }
-	
-	public static void main(String args[])
-	{
-		Menu m = new Menu();
-		m.setVisible(true);
+
+	//button action
+	public void actionPerformed(ActionEvent e) {
+
+		String actionCommand = e.getActionCommand();
+
+		//open game window
+		if (actionCommand.equals("start")) {
+			GameScene Game = new GameScene();
+			Game.setVisible(true);
+
+		} 
+		//open record window
+		else if (actionCommand.equals("record")) {
+			Record record = new Record();
+			record.setVisible(true);
+		}
+
+		//close menu window
+		else if (actionCommand.equals("exit")) {
+			System.exit(0);
+		} else {
+			System.err.println("Error");
+		}
 	}
 
 	
+
 }
